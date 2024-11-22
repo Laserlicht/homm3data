bed888fbf18835158d99fe93c3deef8024eb51f1
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2024-11-22 20:54:34 +0100

    archive org

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,7 +4,7 @@ import tarfile
 from io import BytesIO
 
 if not os.path.isfile(os.path.join(os.path.dirname(__file__), "files/h3bitmap.lod")):
-    url = "https://updates.lokigames.com/loki_demos/heroes3-demo.run"
+    url = "https://web.archive.org/web/20150506062114if_/http://updates.lokigames.com/loki_demos/heroes3-demo.run"
     contents = urllib.request.urlopen(url).read()
     data = contents.split(b"END_OF_STUB\n", 1)[1]
     with tarfile.open(fileobj=BytesIO(data), mode="r:gz") as f:
