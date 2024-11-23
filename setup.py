fe517c5ed5f1f88575aa870fb25ced06a201e282
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2024-11-23 16:01:38 +0100

    publish

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     keywords = "homm3 heroes iii might magic def lod pak",
     url = "https://github.com/Laserlicht/homm3data",
     install_requires=['pillow>=10.3.0', 'numpy>=1.26.4'],
-    setup_requires=['pytest-runner'],
+    setup_requires=['pytest-runner', "twine==5.1.1"],
     tests_require=['pytest==8.3.3'],
     test_suite='tests',
 )
