13edfe9d62cad3591e0380ecd1797e93a728f888
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2026-08-26 23:02:54 +0200

    version

--- tests/__init__.py
+++ tests/__init__.py
@@ -39,7 +39,7 @@ if not os.path.isfile(os.path.join(os.path.dirname(__file__), "files/HotA18/Data
     shutil.rmtree(os.path.join(os.path.dirname(__file__), "files/HotA18/tmp"))
 
 if not os.path.isfile(os.path.join(os.path.dirname(__file__), "files/HotA181/Data/HotA.lod")):
-    url = "https://www.vault.acidcave.net/download/HotA_1.8.1_setup.exe"
+    url = "https://web.archive.org/web/20260826210056if_/https://pixeldrain.com/api/file/xdVEvQ4X?download"
     os.makedirs(os.path.join(os.path.dirname(__file__), "files/HotA181"), exist_ok=True)
     os.makedirs(os.path.join(os.path.dirname(__file__), "files/HotA181/tmp"), exist_ok=True)
     urllib.request.urlretrieve(url, os.path.join(os.path.dirname(__file__), "files/HotA181/tmp/hota.exe"))
